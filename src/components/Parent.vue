d664da1ed64ea25f014d07d625c8e09340232d8d
Marcelo Bettini <bettinimarcelo@gmail.com>
2021-03-10 22:42:47 -0300

    diversas formas de llamar la data de State

--- src/components/Parent.vue
+++ src/components/Parent.vue
@@ -1,15 +1,21 @@
 <template>
   <div>
-    <h4>Number: {{$store.state.number}}</h4>
+    <h4>Number long version: {{ $store.state.number }}</h4>
+    <h4>Number binded to computed prop: {{ num }}</h4>
+    <h4>Number with mapState: {{ number }}</h4>
   </div>
 </template>
 
 <script>
+import { mapState } from "vuex";
 export default {
-
-}
+  computed: {
+    num() {
+      return this.$store.state.number;
+    },
+    ...mapState(["number"]),
+  },
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
--- src/store/index.js
+++ src/store/index.js
@@ -1,23 +1,20 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from "vue";
+import Vuex from "vuex";
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
-    number: 0
+    number: 0,
   },
   mutations: {
     increment(state) {
-      state.number ++;
+      state.number++;
     },
     decrement(state) {
-      state.number --;
-    }
+      state.number--;
+    },
   },
-  actions: {
-    
-  },
-  modules: {
-  }
-})
+  actions: {},
+  modules: {},
+});
