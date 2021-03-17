9977e40cc8f1d74c9c7ae8e2a02c2e55cb2187cd
Marcelo Bettini <marcelobettini@yahoo.com.ar>
2021-03-17 12:30:59 -0300

    pagination refactoring with vuex

--- src/components/Parent.vue
+++ src/components/Parent.vue
@@ -1,22 +1,40 @@
 <template>
+<div>
   <div>
     <h5>Number long version: {{ $store.state.number }}</h5>
     <h5>Number (n() + 1) binded to computed prop: {{ n }}</h5>
     <h5>Number with mapState: {{ number }}</h5>
     <h5>Number (n) with getters (defined at index.js): {{ $store.getters.n }}</h5>
   </div>
+  <div>
+    <h4>Pagination: {{page}} of {{pagination.pages}} pages</h4>
+    <button type="button" @click="prevPage">&lt;&lt;&lt;</button>
+    <button type="button" @click="nextPage">&gt;&gt;&gt;</button>
+  </div>
+  </div>
 </template>
 
 <script>
-import { mapState } from "vuex";
+import { mapState, mapActions } from "vuex";
 
 export default {
   computed: {
     n() {
       return this.$store.state.number + 1;
     },
-    ...mapState(["number"]),
-  },
+    ...mapState(["number", "page", "pagination"]),    
+  },  
+  methods: {
+     nextPage() {
+      this.$store.state.page++;
+      this.getCharacters()
+    },
+    prevPage() {
+      this.$store.state.page--;
+      this.getCharacters()
+    },    
+    ...mapActions(['getCharacters'])
+  }
 };
 </script>
 
--- src/store/index.js
+++ src/store/index.js
@@ -7,6 +7,8 @@ export default new Vuex.Store({
   state: {
     number: 0,
     characters: [],
+    pagination: [],
+    page: 1
   },
   getters: {
     n: (state) => state.number,
@@ -17,16 +19,16 @@ export default new Vuex.Store({
     },
     decrement(state, n) {
       state.number -= n;
-    },
+    },   
     loadCharacters(state, char) {      
       state.characters = char.results;
+      state.pagination = char.info;
     },
   },
   actions: {
     getCharacters: async function({ commit }) {
-      const data = await fetch(`https://rickandmortyapi.com/api/character`);
-      const char = await data.json();
-      console.log(char)
+      const data = await fetch(`https://rickandmortyapi.com/api/character?page=${this.state.page}`);
+      const char = await data.json();      
       commit('loadCharacters', char )
     },
   },
